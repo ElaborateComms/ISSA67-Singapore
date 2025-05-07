b1be4cef2e60ff18d11a6feb354df1718f40f328
Elaborate Communications <bmorrell@elaboratecomms.com>
2025-05-07 17:19:06 +0100

    Update functions.js

--- functions.js
+++ functions.js
@@ -13,3 +13,25 @@ bckBTN.addEventListener("click", () => {
     slideContainer.scrollLeft += 900;
 });
 
+function myFunction(x) {
+    FDBTN.addEventListener("click", () => {
+        slideContainer.style.scrollBehavior = "smooth";
+        slideContainer.scrollLeft -= 450;
+    });
+    
+    bckBTN.addEventListener("click", () => {
+        slideContainer.style.scrollBehavior = "smooth";
+        slideContainer.scrollLeft += 450;
+    });
+  }
+  
+  // Create a MediaQueryList object
+  let x = window.matchMedia("(max-width: 700px)")
+  
+  // Call listener function at run time
+  myFunction(x);
+  
+  // Attach listener function on state changes
+  x.addEventListener("change", function() {
+    myFunction(x);
+  });
